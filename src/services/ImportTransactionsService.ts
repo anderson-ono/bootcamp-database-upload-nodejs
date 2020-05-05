49e60fbb27a4afb1e7fdb28a3c5d40ed73e53582
Anderson <andersonono92@hotmail.com>
2020-05-05 03:51:47 -0300

    Alteração na importação

--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,11 +1,11 @@
 import { getCustomRepository, In } from 'typeorm';
 import csvParse from 'csv-parse';
 import fs from 'fs';
-import Transaction from '../models/Transaction';
 import Category from '../models/Category';
+import Transaction from '../models/Transaction';
 
-import TransactionsRepository from '../repositories/TransactionsRepository';
 import CategoriesRepository from '../repositories/CategoriesRepository';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
 interface CSVTransaction {
   title: string;
